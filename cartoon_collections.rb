cb8db504f189a1c1c1543ff81b3fb710c0d40972
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-26 16:51:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(arr)
    }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  
 end
 
 def find_the_cheese# code an argument here
