e209f6bde0f11da3cd8a3e9a0f0d5971984442c6
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-26 17:10:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if arr.any? { |food| cheese_types.include?(food) } == true
-    return cheese_types
+    return cheese_types[food]
   else
     return nil
   end
