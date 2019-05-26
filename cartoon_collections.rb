7976830d784912eeaa0e3c2ffadfc8407d1cb093
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-26 16:44:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  arr.map { |w| puts "#{w.capitalize}! " }
+  arr.each { |w| w.capitalize} + "! " }
 end
 
 def long_planeteer_calls# code an argument here
